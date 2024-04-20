63aac0fe1751b22cdb0d4692c357332d62ac460f
KMLPr <165916906+KMLPr@users.noreply.github.com>
2024-04-20 08:52:03 -0400

    ShoppingCart Object Implementation in User
    The User class was now updated to include the ShoppingCart object which will be unique to each user. Testing will need to be done in the Main class to ensure the output is correct for this.

---
+++ User Version With ShoppingCart.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+public class User {
+    private String customerID;
+    private HashMap<String, String> userData;
+    private ShoppingCart shoppingCart;
+
+    public User(String username, String hashedPassword){
+        this.customerID = username.hashCode() + "-" + hashedPassword.hashCode();
+        this.userData = new HashMap<>();
+        this.shoppingCart = new ShoppingCart();
+        userData.put("username", username);
+        userData.put("password", hashedPassword);
+    }
+    public String getCustomerId() {
+        return customerID;
+    }
+    public ShoppingCart getShoppingCart(){
+        return shoppingCart;
+    }
+
+    public String getUsername() {
+        return userData.get("username");
+    }
+
+    public String getHashedPassword() {
+        return userData.get("password");
+    }
+
+    // Additional methods to access and modify user data
+    public String getUserData(String key) {
+        return userData.get(key);
+    }
+
+    public void setUserData(String key, String value) {
+        userData.put(key, value);
+    }
+
+    public String toString(){
+        String info = "Username: " + getUsername() + " and Password: " + getHashedPassword() + " and customer ID: ";
+        info += getCustomerId();
+
+        return info;
+    }
+}
+
+
