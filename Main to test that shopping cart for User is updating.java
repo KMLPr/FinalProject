5e28acb2f46881d5cf2f410b898db636ec4b2457
KMLPr <165916906+KMLPr@users.noreply.github.com>
2024-04-20 09:22:03 -0400

    Testing Update Functionality
    I am now testing the updating method in the User class to update the given users shopping cart and make changes to it. To test this I used a new ShoppingCart variable to get the user shopping cart again, remove an object, calculate the new price, and return the current products in their shopping cart now. This was the output from it:
    All products in user shopping cart: 
    [
    Product Name: Laptop
    Description: electronic
    Price: $999.99
    , 
    Product Name: Car
    Description: toy
    Price: $19.99
    ]
    Total price: 1019.98
    All products in updated user shopping cart: 
    [
    Product Name: Car
    Description: toy
    Price: $19.99
    ]
    Total price: 19.99

---
+++ Main to test that shopping cart for User is updating.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        Product product1 = new Product("Laptop", "electronic", 999.99);
+        Product product2 = new Product("Smartphone", "electronic", 599.99);
+        Product product3 = new Product("Pizza", "food", 9.99);
+        Product product4 = new Product("Car", "toy", 19.99);
+        Product product5 = new Product("Car", "vehicle", 125000.00);
+
+        // Create a product catalog
+        ProductCatalog productCatalog = new ProductCatalog();
+
+        // Add products to the catalog
+        productCatalog.addProduct(product1);
+        productCatalog.addProduct(product2);
+        productCatalog.addProduct(product3);
+        productCatalog.addProduct(product4);
+        productCatalog.addProduct(product5);
+
+        UserManagement userManagement = new UserManagement();
+
+        User user = new User("lovegood", "123", productCatalog);
+        ShoppingCart userShopping = user.getShoppingCart();
+        userShopping.addProduct(product1.getName());
+        userShopping.addProduct(product4.getName());
+        System.out.println("All products in user shopping cart: ");
+        System.out.println(userShopping.getProducts());
+        System.out.println("Total price: " + userShopping.calculateTotalPrice());
+        user.updateCart(userShopping);
+
+        ShoppingCart updateCart = user.getShoppingCart();
+        updateCart.removeProduct(product1.getName());
+        System.out.println("All products in updated user shopping cart: ");
+        System.out.println(updateCart.getProducts());
+        System.out.println("Total price: " + updateCart.calculateTotalPrice());
+        user.updateCart(userShopping);
+    }
+}
