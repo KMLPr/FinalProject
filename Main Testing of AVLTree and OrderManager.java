f1c9e7b5866a3be0ca3318c32f33965faf128588
KMLPr <165916906+KMLPr@users.noreply.github.com>
2024-04-22 08:19:17 -0400

    Testing the Functionality of the AVLTree
    This is the first test for the AVLTree to make sure that it prints out the Orders in ascending order based on their price. This first test includes inserting 3 different Orders into the AVLTree, printing the tree, removing an Order, and then printing out the tree again to make sure the Order was removed and the AVLTree handled the removal by rebalancing itself. Based on the prices I expect to have an output of Order 2, Order 1, and Order 3 in that order and once I removed Order 2 (the root node) Order 1 should then become the root node of Order 3.
    Output:
    AVL Tree (Inorder Traversal):
    Order ID: 2
    User: user2
    Products: [
    Product Name: Product3
    Description: book
    Price: $20.99
    , 
    Product Name: Product4
    Description: game
    Price: $25.99
    ]
    Price: 46.98
     Order ID: 1
    User: user1
    Products: [
    Product Name: Product1
    Description: car
    Price: $40.99
    , 
    Product Name: Product2
    Description: toy
    Price: $15.99
    ]
    Price: 56.980000000000004
     Order ID: 3
    User: user3
    Products: [
    Product Name: Product5
    Description: game
    Price: $30.99
    , 
    Product Name: Product6
    Description: car
    Price: $35.99
    ]
    Price: 66.98
     
    AVL Tree after Removal:
    Order ID: 1
    User: user1
    Products: [
    Product Name: Product1
    Description: car
    Price: $40.99
    , 
    Product Name: Product2
    Description: toy
    Price: $15.99
    ]
    Price: 56.980000000000004
     Order ID: 3
    User: user3
    Products: [
    Product Name: Product5
    Description: game
    Price: $30.99
    , 
    Product Name: Product6
    Description: car
    Price: $35.99
    ]
    Price: 66.98
    
    Memory Usage: 375 M of 2048 M
    Time Elapsed: 1 sec, 828 ms

---
+++ Main Testing of AVLTree and OrderManager.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("user1", "1");
+        User user2 = new User("user2", "2");
+        User user3 = new User("user3", "3");
+
+        // Create some product lists
+        ArrayList<Product> productList1 = new ArrayList<>();
+        productList1.add(new Product("Product1", "car", 40.99));
+        productList1.add(new Product("Product2", "toy", 15.99));
+
+        ArrayList<Product> productList2 = new ArrayList<>();
+        productList2.add(new Product("Product3", "book", 20.99));
+        productList2.add(new Product("Product4", "game", 25.99));
+
+
+        ArrayList<Product> productList3 = new ArrayList<>();
+        productList3.add(new Product("Product5", "game", 30.99));
+        productList3.add(new Product("Product6", "car", 35.99));
+
+
+        // Create an AVL tree to manage orders
+        OrderManager orderManager = new OrderManager();
+        orderManager.printOrders();
+
+        // Insert some orders
+        orderManager.addOrder(new Order(1, user1, productList1));
+        orderManager.addOrder(new Order(2, user2, productList2));
+        orderManager.addOrder(new Order(3, user3, productList3));
+
+        // Print the AVL tree (inorder traversal)
+        System.out.println("AVL Tree (Inorder Traversal):");
+        orderManager.printOrders();
+        System.out.println();
+
+        // Remove an order
+        orderManager.removeOrder(2);
+
+        // Print the AVL tree after removal
+        System.out.println("AVL Tree after Removal:");
+        orderManager.printOrders();
+
+    }
+}
